531a19f8a6c2e2e5c51b6bf28eec7c8ef06e4746
Hyungsuk Kim <76229107+sesame0717@users.noreply.github.com>
2021-12-23 08:24:37 -0800

    last minute change

--- src/tests/tests_dao/test_manager_dao.py
+++ src/tests/tests_dao/test_manager_dao.py
@@ -3,11 +3,11 @@ from src.data_access_layer.implementation_classes.manager_dao import ManagerDao
 manager_dao = ManagerDao()
 
 def test_approve_reim_by_id():
-    passed = manager_dao.approve_reim_by_id(1, "accident in company property")
+    passed = manager_dao.approve_reim_by_id(2, "accident in company property")
     assert passed
 
 def test_reject_reim_by_id():
-    passed = manager_dao.reject_reim_by_id(2, "unauthorized action")
+    passed = manager_dao.reject_reim_by_id(1, "unauthorized action")
     assert passed
 
 def test_get_all_reims():
